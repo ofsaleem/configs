7273e2905010ebbad161581a932e1b68ecdc85c3
Omar Saleem <theshadowzero@gmail.com>
2016-04-02 23:19:30 -0400

    reworked configs to work with new graphics card, fixed screen tearing, fixed dpi, etc

--- monitorsetup.sh
+++ monitorsetup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # sets up my monitors. hopefully the tv works hotpluggy 
-xrandr --setprovideroutputsource modesetting NVIDIA-0 \
- --output HDMI-1-1 --mode 1680x1050 --rate 60 --pos 0x430 \
- --output DVI-I-1 --pos 1680x360 --rate 60 --primary \
- --output DVI-D-0 --pos 3601x0 --rate 60 --rotate left
+xrandr --output DP-3 --mode 1680x1050 --rate 60 --pos 0x430 --output DP-0 --pos 1680x360 --rate 60 --primary --output DVI-I-1 --pos 3600x0 --rate 60 --rotate left --dpi 96x96 
 # TV may not be on so we don't want the whole command to fail!
 #xrandr --output HDMI-0 --mode 1360x768 --pos 4682x576
+
+
 
